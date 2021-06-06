ce55beb7cf6ee489a1315f94f8658d577b424a5c
Bilbo <bilbo.sacquet@orange.fr>
2021-06-06 18:38:58 +0200

    updating premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -3,6 +3,7 @@ project "AtlasEngine"
 	language "C++"
 	cppdialect "C++17"
 	staticruntime "on"
+	architecture "x86_64"
 
 	targetdir ("%{prj.location}/build/" .. outputdir)
 	objdir ("%{prj.location}/build-int/" .. outputdir)
@@ -11,46 +12,15 @@ project "AtlasEngine"
 	pchsource "src/AEpch.cpp"
 	flags "NoPCH"
 	linkoptions "-shared"
-	
---[[
-	platforms {
-        "Win32",
-        "Win64",
-        "macos",
-        "linux86",
-        "linux86_64",
-        -- etc.
-    }
-	filter {"platforms:Win32"}
-		system "Windows"
-		architecture "x86"
-		libdirs "./libs/lib/Windows/mingw/x86_64"
 
-	filter {"platforms:Win64"}
-		system "Windows"
-		architecture "x86_64"
-		libdirs "./libs/lib/Windows/mingw/x86_64"
+	filter "system:windows"
+		libdirs "libs/lib/Windows"
 
-	filter {"platforms:linux86"}
-		system "linux"
-		architecture "x86"
-		libdirs "./libs/lib/Linux/x86_64"
+	filter "system:linux"
+		libdirs "libs/lib/Linux"
 
-	filter {"platforms:linux86_64"}
-		system "linux"
-		architecture "x86_64"
-		libdirs "./libs/lib/Linux/x86_64"
-	
-	filter {"platforms:macos"}
-		system "macosx"
---]]
-
-	libdirs "%{prj.location}/libs/lib/Linux/x86_64"
-	
 	links
 	{
-		"GLEW",
-		"GL",
 		"vulkan",
 		"SDL2",
 		"SDL2_image",
@@ -64,7 +34,7 @@ project "AtlasEngine"
 		"src/**.cpp"
 	}
 
-	defines {"GLEW_STATIC", "SDL_MAIN_HANDLED"}
+	defines "SDL_MAIN_HANDLED"
 
 	includedirs 
 	{
