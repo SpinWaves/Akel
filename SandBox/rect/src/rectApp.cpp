56ddb376bb3c2f4f4b01ef699896e378107a7ee4
Kbz-8 <71275324+Kbz-8@users.noreply.github.com>
2022-03-01 06:28:10 +0100

    adding fps printer to rect demo

--- SandBox/rect/src/rectApp.cpp
+++ SandBox/rect/src/rectApp.cpp
@@ -1,18 +1,25 @@
 #include <Akel.h>
 #include <Akel_main.h>
 
+class FPSprinterComponent : public Ak::Component
+{
+    public:
+        void update() override { Ak::CounterFPS::printTicks(); Ak::CounterFPS::printFPS(); }
+};
+
 class Rect : public Ak::Application
 {
     public:
         explicit Rect() : Ak::Application("Rect")
         {
             Ak::WindowComponent* window = Ak::custom_malloc<Ak::WindowComponent>();
-            add_component(window); // inherited from Ak::Application
+            add_component(window); // function inherited from Ak::Application
             window->setSetting(Ak::winsets::title, "Rectangle using Akel Engine");
             //window->setSetting(Ak::winsets::size, 1280, 750);
             window->setSetting(Ak::winsets::resizable, false);
 
             add_component(Ak::custom_malloc<Ak::RendererComponent>(window->getNativeWindow()));
+            add_component(Ak::custom_malloc<FPSprinterComponent>());
         }
         ~Rect() = default;
 };
