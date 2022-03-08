eeab38de1a730cae8f68bb7973d63443669dffda
Kbz-8 <71275324+Kbz-8@users.noreply.github.com>
2022-03-08 17:56:15 +0100

    pushing code

--- SandBox/cube/src/cubeApp.cpp
+++ SandBox/cube/src/cubeApp.cpp
@@ -16,17 +16,25 @@ class Cube : public Ak::Application
             add_component(window); // function inherited from Ak::Application
             window->setSetting(Ak::winsets::title, "Cube using Akel Engine");
             window->setSetting(Ak::winsets::resizable, true);
-            window->setSetting(Ak::winsets::vsync, true);
+            window->setSetting(Ak::winsets::vsync, false);
             window->setSetting(Ak::winsets::size, 1280, 750);
 
             add_component<FPSprinterComponent>();
-            add_component<Ak::Camera3D>();
+            add_component<Ak::Camera3D>(-1, -1, 10);
 
             Ak::RendererComponent* renderer = Ak::memAlloc<Ak::RendererComponent>(window, Ak::shader::basic_3D);
-            renderer->setClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-            Ak::Entity2D rectangle(Models::quad, {0.0f, 0.0f}, {1.0f, 1.0f}, Colors::blue);
-            renderer->add_entity(rectangle);
             add_component(renderer);
+            renderer->setBackgroundColor(0.49f, 0.66f, 0.85f, 1.0f);
+
+            Ak::Entity2D rectangle(Models::quad, {0.0f, 0.0f}, {1.0f, 5.0f}, Colors::black);
+            Ak::Entity2D rectangle2(Models::quad, {1.0f, 0.0f}, {1.0f, 5.0f}, Colors::yellow);
+            Ak::Entity2D rectangle3(Models::quad, {2.0f, 0.0f}, {1.0f, 5.0f}, Colors::red);
+            
+            renderer->add_entity(rectangle);
+            renderer->add_entity(rectangle2);
+            renderer->add_entity(rectangle3);
+
+            renderer->recreateRenderer();
         }
         ~Cube() = default;
 };
--- SandBox/rect/src/rectApp.cpp
+++ SandBox/rect/src/rectApp.cpp
@@ -16,15 +16,18 @@ class Rect : public Ak::Application
             add_component(window); // function inherited from Ak::Application
             window->setSetting(Ak::winsets::title, "Rectangle using Akel Engine");
             window->setSetting(Ak::winsets::resizable, true);
-            window->setSetting(Ak::winsets::vsync, true);
+            window->setSetting(Ak::winsets::vsync, false);
             window->setSetting(Ak::winsets::size, 1280, 750);
 
             add_component<FPSprinterComponent>();
 
             Ak::RendererComponent* renderer = Ak::memAlloc<Ak::RendererComponent>(window, Ak::shader::basic_2D);
-            renderer->setClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-            Ak::Entity2D rectangle(Models::quad, {-0.9f, -0.9f}, {0.5f, 0.5f}, Colors::red);
+            renderer->setRenderingMode(Ak::RenderingMode::render2D);
+            renderer->setBackgroundColor(0.49f, 0.66f, 0.85f, 1.0f);
+
+            Ak::Entity2D rectangle(Models::quad, {50.0f, 50.0f}, {250.0f, 200.0f}, Colors::red);
             renderer->add_entity(rectangle);
+            
             add_component(renderer);
         }
         ~Rect() = default;
