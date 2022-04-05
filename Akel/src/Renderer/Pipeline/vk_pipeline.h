03e262c80b806a1c0c0c2a4bfca09c15d773bc5d
kbz_8 <kbz_8.dev@akel-engine.com>
2022-04-05 13:24:34 +0200

    OzO

--- Akel/src/Renderer/Pipeline/vk_pipeline.h
+++ Akel/src/Renderer/Pipeline/vk_pipeline.h
@@ -1,7 +1,7 @@
 // This file is a part of Akel
 // Authors : @kbz_8
 // Created : 04/04/2022
-// Updated : 04/04/2022
+// Updated : 05/04/2022
 
 #ifndef __AK_VK_PIPELINE__
 #define __AK_VK_PIPELINE__
@@ -15,10 +15,17 @@ namespace Ak
 	class Pipeline
 	{
 		public:
+			void init();
+			inline void destroy() noexcept 
+			{
+				vkDestroyPipeline(Render_Core::get().getDevice()->get(), _graphicsPipeline, nullptr);
+				vkDestroyPipelineLayout(Render_Core::get().getDevice()->get(), _pipelineLayout, nullptr);
+			}
 
 		private:
 			VkPipelineLayout _pipelineLayout;
 			VkPipeline _graphicsPipeline;
+			VkPipelineCache _cache;
 	};
 }
 
