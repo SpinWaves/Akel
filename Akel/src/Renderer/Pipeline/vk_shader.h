b7247f2f2934d9a57b919a1b99d9b516316397f6
kbz_8 <kbz_8.dev@akel-engine.com>
2022-04-15 11:48:31 +0200

    working on shaders

--- Akel/src/Renderer/Pipeline/vk_shader.h
+++ Akel/src/Renderer/Pipeline/vk_shader.h
@@ -16,7 +16,58 @@ namespace Ak
 	class Shader
 	{
 		public:
-			enum class type { vertex, fragment }; // TODO : add more shaders types support
+			class Uniform
+			{
+				public:
+
+				private:
+			};
+
+			class Uniform_block
+			{
+				public:
+
+				private:
+			};
+
+			class Vertex_input
+			{
+				public:
+					Vertex_input(std::vector<VkVertexInputBindingDescription> bindings = {}, std::vector<VkVertexInputAttributeDescription> attributes = {}) : _bindings(std::move(bindings)), _attributes(std::move(attributes)) {}
+
+					const std::vector<VkVertexInputBindingDescription>& getBindingDescriptions() const { return _binding; }
+					const std::vector<VkVertexInputAttributeDescription>& getAttributeDescriptions() const { return _attributes; }
+
+				private:
+					std::vector<VkVertexInputBindingDescription> _bindings;
+					std::vector<VkVertexInputAttributeDescription> _attributes;
+			};
+
+			class Attribute
+			{
+				public:
+					Attribute(int32_t binding, int32_t location, int32_t offset, int32_t format) : _binding(binding), _location(location), _offset(offset), _format(format) {}
+
+					inline int32_t getBinding() const noexcept { return _binding; }
+					inline int32_t getLocation() const noexcept { return _location; }
+					inline int32_t getOffest() const noexcept { return _offset; }
+					inline int32_t getFormat() const noexcept { return _format; }
+
+					inline bool operator==(const Attribute &rhs) const noexcept { return _binding == rhs._binding && _location == rhs._location && _offset == rhs._offset && _format == rhs._format; }
+					inline bool operator!=(const Attribute &rhs) const noexcept { return !operator==(rhs); }
+
+					inline VkVertexInputAttributeDescription convert_to_vk_vertex_input_attribute() const noexcept { return VkVertexInputAttributeDescription{_binding, _location, _format, _offset}; }
+
+					Attribute() = default;
+
+				private:
+					int32_t _binding = 0;
+					int32_t _location = 0;
+					int32_t _offset = 0;
+					int32_t _format = 0;
+			};
+
+			enum class type { vertex, fragment, geometry, tesselation };
 
 			Shader(const fString path, type t);
 
