c4b4d2a666f7fb8bfbd04bfaa6ca4ba503b27629
Kbz-8 <kbz_8.dev@akel-engine.com>
2022-09-30 02:03:14 +0200

    working on Materials part of Akel Studio

--- Akel_Studio/src/Panels/materials.cpp
+++ Akel_Studio/src/Panels/materials.cpp
@@ -1,12 +1,12 @@
 // This file is a part of Akel Studio
 // Authors : @kbz_8
 // Created : 11/03/2022
-// Updated : 29/09/2022
+// Updated : 30/09/2022
 
 #include <Panels/materials.h>
 #include <Fonts/material_font.h>
 
-Materials::Materials(std::shared_ptr<Ak::ELTM> eltm) : Panel("__materials")
+Materials::Materials(std::shared_ptr<Ak::ELTM> eltm) : Panel("__materials"), _new_material(false)
 {
     _eltm = std::move(eltm);
 }
@@ -15,21 +15,48 @@ void Materials::onUpdate(Ak::Maths::Vec2<int>& size)
 {
     if(ImGui::Begin(std::string(AKS_ICON_MD_CATEGORY" " + _eltm->getText("Materials.name")).c_str(), nullptr, ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize))
     {
-		if(ImGui::BeginChild("Materials", ImVec2(ImGui::GetWindowWidth() - 15.0f, ImGui::GetWindowHeight() - 90.0f), true, ImGuiWindowFlags_HorizontalScrollbar))
+		if(ImGui::BeginChild("Materialsyy", ImVec2(ImGui::GetWindowWidth() - 15.0f, ImGui::GetWindowHeight() - 90.0f), true, ImGuiWindowFlags_HorizontalScrollbar))
 		{
-			ImGui::Dummy(ImVec2(42.0f, (ImGui::GetWindowHeight() - 90.0f) / 2.0f));
-			
-			ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1.f, 1.f, 1.f, 0.5f));
-				ImGui::Indent(((ImGui::GetWindowWidth() - 15.0f) / 2.0f) - ImGui::CalcTextSize(_eltm->getText("Materials.no_materials").c_str()).x / 2.0f);
-				ImGui::TextUnformatted(_eltm->getText("Materials.no_materials").c_str());
-			ImGui::PopStyleColor();
+			if(_names.empty())
+			{
+				ImGui::Dummy(ImVec2(42.0f, (ImGui::GetWindowHeight() - 90.0f) / 2.0f));
+				ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1.f, 1.f, 1.f, 0.5f));
+					ImGui::Indent(((ImGui::GetWindowWidth() - 15.0f) / 2.0f) - ImGui::CalcTextSize(_eltm->getText("Materials.no_materials").c_str()).x / 2.0f);
+					ImGui::TextUnformatted(_eltm->getText("Materials.no_materials").c_str());
+				ImGui::PopStyleColor();
+			}
 
 			ImGui::EndChild();
 		}
 		ImGui::Separator();
 		if(ImGui::Button(std::string(_eltm->getText("Materials.add") + " "AKS_ICON_MD_ADD).c_str(), ImVec2(ImGui::GetWindowWidth() - 15.0f, ImGui::GetFontSize() * 2.0f)))
-		{
-		}
+			_new_material = true;
         ImGui::End();
     }
+
+	if(_new_material)
+	{
+		ImGui::SetNextWindowPos(ImVec2(size.X / 2.0f - 200.0f, size.Y / 2.0f - 200.0f));
+		ImGui::SetNextWindowSize(ImVec2(400.0f, 400.0f));
+		ImGui::SetNextWindowFocus();
+		if(ImGui::Begin(std::string(AKS_ICON_MD_CATEGORY" " + _eltm->getText("Materials.add")).c_str(), &_new_material, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize))
+		{
+			static char name[128] = { 0 };
+			ImGui::TextUnformatted(_eltm->getText("Materials.mat_name").c_str());
+			ImGui::SameLine();
+            ImGui::InputText("##material_name", name, 128);
+
+			ImGui::Dummy(ImVec2(69.0f, ImGui::GetWindowHeight() - ImGui::GetFontSize() * 6.5f));
+			ImGui::Separator();
+
+			if(ImGui::Button(std::string(_eltm->getText("Materials.create") + " "AKS_ICON_MD_NOTE_ADD).c_str(), ImVec2(ImGui::GetWindowWidth() - 15.0f, ImGui::GetFontSize() * 2.0f)) && std::strlen(name) != 0)
+			{
+				_names.emplace_back(name);
+				std::memset(name, 0, 128);
+				_new_material = false;
+			}
+
+			ImGui::End();
+		}
+	}
 }
--- Akel_Studio/src/Panels/materials.h
+++ Akel_Studio/src/Panels/materials.h
@@ -1,7 +1,7 @@
 // This file is a part of Akel Studio
 // Authors : @kbz_8
 // Created : 11/03/2022
-// Updated : 28/09/2022
+// Updated : 30/09/2022
 
 #ifndef __AK_STUDIO_MATERIALS__
 #define __AK_STUDIO_MATERIALS__
@@ -17,6 +17,10 @@ class Materials : public Panel
         void onUpdate(Ak::Maths::Vec2<int>& size) override;
 
         ~Materials() = default;
+
+	private:
+		std::vector<Ak::fString> _names;
+		bool _new_material;
 };
 
 #endif // __AK_STUDIO_MATERIALS__
