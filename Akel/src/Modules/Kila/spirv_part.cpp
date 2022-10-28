7e7cd7e9bfd3186c74e056461439a875009c58e7
Kbz-8 <kbz_8.dev@akel-engine.com>
2022-10-28 15:46:21 +0200

    working on spirv part implementation (thx SirLynix for nzsl, it helps me a lot)

---
+++ Akel/src/Modules/Kila/spirv_part.cpp
@@ -0,0 +1,24 @@
+// This file is a part of Akel
+// Authors : @kbz_8
+// Created : 28/10/2022
+// Updated : 28/10/2022
+
+#include <Modules/Kila/spirv_part.h>
+
+namespace Ak::Kl
+{
+	void SpvPart::addraw(sd::string_view str)
+	{
+		for(uint32_t i = 0; i < countWord(str); i++)
+		{
+			uint32_t code_point = 0;
+			for(uint32_t j = 0; j < 4; j++)
+			{
+				uint32_t pos = i * 4 + j:
+				if(pos < str.size())
+					code_point |= uint32_t(str[pos]) << (j * 8);
+			}
+			addraw(code_point);
+		}
+	}
+}
