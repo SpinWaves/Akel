b4cc55d5779fde710004365e21f73c5d7d38e161
Kbz-8 <kbz_8.dev@akel-engine.com>
2022-11-16 17:26:19 +0100

    adding scene

---
+++ Akel/src/Scene/scene.h
@@ -0,0 +1,21 @@
+// This file is a part of Akel
+// Authors : @kbz_8
+// Created : 16/11/2022
+// Updated : 16/11/2022
+
+#ifndef __AK_SCENE__
+#define __AK_SCENE__
+
+#include <Core/Components/baseComponent.h>
+
+namespace Ak
+{
+	class Scene : public Component
+	{
+		public:
+
+		private:
+	};
+}
+
+#endif
