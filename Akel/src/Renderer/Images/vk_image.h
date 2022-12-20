f4810031b28da2854b3605c9af4108904521cd4f
Kbz-8 <kbz_8.dev@akel-engine.com>
2022-12-20 23:11:28 +0100

    wow, a lot of work

--- Akel/src/Renderer/Images/vk_image.h
+++ Akel/src/Renderer/Images/vk_image.h
@@ -1,7 +1,7 @@
 // This file is a part of Akel
 // Authors : @kbz_8
 // Created : 19/12/2022
-// Updated : 19/12/2022
+// Updated : 20/12/2022
 
 #ifndef __AK_VK_IMAGE__
 #define __AK_VK_IMAGE__
@@ -15,6 +15,7 @@ namespace Ak
 		public:
 
 		private:
+			VkImage _image = VK_NULL_HANDLE;
 	};
 }
 
--- Akel/src/Renderer/SwapChain/vk_swapchain.h
+++ Akel/src/Renderer/SwapChain/vk_swapchain.h
@@ -1,7 +1,7 @@
 // This file is a part of Akel
 // Authors : @kbz_8
 // Created : 04/04/2022
-// Updated : 19/12/2022
+// Updated : 20/12/2022
 
 #ifndef __AK_VK_SWAPCHAIN__
 #define __AK_VK_SWAPCHAIN__
@@ -53,10 +53,6 @@ namespace Ak
             VkExtent2D _swapChainExtent;
             std::vector<FrameBuffer*> _framebuffers;
             std::vector<ImageView*> _imageViews;
-
-			VkImage depthImage;
-			VkDeviceMemory depthImageMemory;
-			VkImageView depthImageView;
     };
 }
 
