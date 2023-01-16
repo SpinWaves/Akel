1d3107cc1ab591eaf05c282d6c8c238bd60ee1f2
Kbz-8 <kbz_8.dev@akel-engine.com>
2023-01-16 03:22:49 +0100

    begenning resources manager

---
+++ Akel/src/Platform/resourceManager.h
@@ -0,0 +1,25 @@
+// This file is a part of Akel
+// Authors : @kbz_8
+// Created : 16/01/2023
+// Updated : 16/01/2023
+
+#ifndef __AK_RESOURCE_MANAGER__
+#define __AK_RESOURCE_MANAGER__
+
+#include <Akpch.h>
+#include <Utils/selfInstance.h>
+
+namespace Ak
+{
+	class Res : public SelfInstance<Res>
+	{
+		public:
+			Res() = default;
+
+			~Res() = default;
+
+		private:
+	};
+}
+
+#endif
