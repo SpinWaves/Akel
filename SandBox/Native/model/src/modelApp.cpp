efc56b096028472ed1d970db8834b4899353f408
Kbz-8 <kbz_8.dev@akel-engine.com>
2023-03-05 11:49:34 +0100

    yes

--- SandBox/Native/model/src/modelApp.cpp
+++ SandBox/Native/model/src/modelApp.cpp
@@ -16,6 +16,13 @@ Ak::Application* Akel_mainApp(Ak::CommandLineArgs args)
 	scene->addCamera<Ak::Cam::FirstPerson3D>(-5.0f, 1.0f, 0.0f);
 	app->add_scene(scene);
 
+	Ak::MaterialDesc lem_material_desc;
+	lem_material_desc.albedo = Ak::Res::get().getTexturesPath() / "apollo_map.jpg";
+	Ak::MaterialID lem_material = Ak::MaterialLibrary::get().addMaterialToLibrary(lem_material_desc);
+
+	Ak::Entity lem = scene->createEntity();
+	lem.addAttribute<Ak::ModelAttribute>(Ak::Res::get().getMeshesPath() / "apollo_lunar_module.obj", lem_material);
+
 	Ak::MaterialDesc knuckles_material_desc;
 	knuckles_material_desc.albedo = Ak::Res::get().getTexturesPath() / "knuckles.png";
 	Ak::MaterialID knuckles_material = Ak::MaterialLibrary::get().addMaterialToLibrary(knuckles_material_desc);
