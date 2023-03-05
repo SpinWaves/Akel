6d86bf6fb4260bbd722a5259008a078bc5e5911e
Kbz-8 <kbz_8.dev@akel-engine.com>
2023-03-05 16:09:50 +0100

    fixing compilation error on 32bits Vulkan version

--- Akel/include/Modules/ImGui/imgui_utils.h
+++ Akel/include/Modules/ImGui/imgui_utils.h
@@ -1,7 +1,7 @@
 // This file is a part of Akel
 // Authors : @kbz_8
 // Created : 17/08/2022
-// Updated : 22/02/2023
+// Updated : 05/03/2023
 
 #ifndef __AK_IMGUI_UTILS__
 #define __AK_IMGUI_UTILS__
@@ -20,7 +20,7 @@ namespace AkImGui
 		public:
 			ImImage() = default;
 
-			inline ImTextureID getImGuiID() noexcept { return static_cast<ImTextureID>(_set); }
+			inline ImTextureID getImGuiID() noexcept { return (ImTextureID)_set; }
 			void destroy();
 
 			~ImImage() = default;
